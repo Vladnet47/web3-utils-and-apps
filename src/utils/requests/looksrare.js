e48eb214ba2891e1d0ad4778dc8702c144725d92
Vladnet47 <vlad.netrebchenko@gmail.com>
2022-05-15 23:43:32 -0700

    Added looks api calls, including retrieving nonce and creating listings

--- src/utils/requests/looksrare.js
+++ src/utils/requests/looksrare.js
@@ -1,63 +1,191 @@
 const ethers = require('ethers');
 const HttpRequests = require('./http');
 
-class LooksRequests extends HttpRequests {
-    async getListing(tokenContract, tokenId) {
-        if (!tokenContract) {
-            throw new Error('Missing token contract');
-        }
-        if (tokenId == null) {
-            throw new Error('Missing token id');
-        }
+const URL = 'https://api.looksrare.org/graphql';
+const HEADERS = {
+    'authority': 'api.looksrare.org',
+    'method': 'POST',
+    'path': '/graphql',
+    'scheme': 'https',
+    'authorization': 'Bearer undefined',
+    'accept': '*/*',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/json',
+    'dnt': '1',
+    'origin': 'https://looksrare.org',
+    'referer': 'https://looksrare.org/',
+    'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
+};
 
-        const url = 'https://api.looksrare.org/graphql';
-        const headers = {
-            'authority': 'api.looksrare.org',
-            'method': 'POST',
-            'path': '/graphql',
-            'scheme': 'https',
-            'accept': '*/*',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9',
-            'content-type': 'application/json',
-            'dnt': '1',
-            'origin': 'https://looksrare.org',
-            'referer': 'https://looksrare.org/',
-            'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'empty',
-            'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'same-site',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
-        };
+const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+const STRATEGY = '0x56244Bb70CbD3EA9Dc8007399F61dFC065190031';
+const EXCHANGE = '0x59728544B08AB483533076417FbBB2fD0B17CE3a';
+const MAKER_ORDER_SIG_DOMAIN = {
+    name: 'LooksRareExchange',
+    version: '1',
+    chainId: 1,
+    verifyingContract: EXCHANGE,
+};
+const MAKER_ORDER_SIG_TYPES = {
+    MakerOrder: [
+        { name: "isOrderAsk", type: "bool" },
+        { name: "signer", type: "address" },
+        { name: "collection", type: "address" },
+        { name: "price", type: "uint256" },
+        { name: "tokenId", type: "uint256" },
+        { name: "amount", type: "uint256" },
+        { name: "strategy", type: "address" },
+        { name: "currency", type: "address" },
+        { name: "nonce", type: "uint256" },
+        { name: "startTime", type: "uint256" },
+        { name: "endTime", type: "uint256" },
+        { name: "minPercentageToAsk", type: "uint256" },
+        { name: "params", type: "bytes" },
+    ],
+};
+
+class LooksRequests extends HttpRequests {
+    async getListing(token) {
+        if (!token) {
+            throw new Error('Missing token');
+        }
 
         const body = {
             query: "\n    query GetToken($collection: Address!, $tokenId: String!) {\n      token(collection: $collection, tokenId: $tokenId) {\n        id\n        tokenId\n        image {\n          src\n          contentType\n        }\n        name\n        countOwners\n        description\n        animation {\n          src\n          contentType\n          original\n        }\n        lastOrder {\n          price\n          currency\n        }\n        collection {\n          name\n          address\n          type\n          isVerified\n          points\n          totalSupply\n          description\n          owner {\n            ...CollectionOwnerFragment\n          }\n          floorOrder {\n            price\n          }\n          floor {\n            floorPriceOS\n            floorPrice\n            floorChange24h\n            floorChange7d\n            floorChange30d\n          }\n        }\n        ask {\n          ...OrderFragment\n        }\n        attributes {\n          ...AttributeFragment\n        }\n      }\n    }\n    \n  fragment AttributeFragment on Attribute {\n    traitType\n    value\n    displayType\n    count\n    floorOrder {\n      price\n    }\n  }\n\n    \n  fragment OrderFragment on Order {\n    isOrderAsk\n    signer\n    collection {\n      address\n    }\n    price\n    amount\n    strategy\n    currency\n    nonce\n    startTime\n    endTime\n    minPercentageToAsk\n    params\n    signature\n    token {\n      tokenId\n    }\n    hash\n  }\n\n    \n  fragment CollectionOwnerFragment on User {\n    address\n    name\n    isVerified\n    avatar {\n      id\n      tokenId\n      image {\n        src\n        contentType\n      }\n    }\n  }\n\n  ",
             variables: {
-                collection: tokenContract.toString(),
-                tokenId: tokenId.toString()
+                collection: token.address.toString(),
+                tokenId: token.id.toString()
+            }
+        };
+
+        const res = await this.post(URL, body, HEADERS, 'json');
+        if (!res || !res.data || !res.data.token) {
+            throw new Error('Response data invalid format');   
+        }
+
+        return {
+            floorPrice: ethers.utils.parseEther(res.data.token.collection.floorOrder.price),
+            floorPriceOS: ethers.utils.parseEther(res.data.token.collection.floor.floorPriceOS),
+            isListed: res.data.token.ask != null,
+            price: res.data.token.ask ? ethers.utils.parseEther(res.data.token.ask.price) : null,
+            nonce: res.data.token.ask ? res.data.token.ask.nonce : null,
+        };
+    }
+
+    // Duration in minutes
+    async createListing(signer, token, price, nonce, duration) {
+        if (!signer || !signer._isSigner) {
+            throw new Error('Missing or invalid signer');
+        }
+        if (!token) {
+            throw new Error('Missing token');
+        }
+        if (!price || !price._isBigNumber) {
+            throw new Error('Missing or invalid price');
+        }
+        if (nonce == null || nonce < 0) {
+            throw new Error('Missing or invalid listing nonce');
+        }
+        if (duration < 1) {
+            throw new Error('Missing or invalid duration');
+        }
+
+        // Generate signature over query parameters
+        const startTime = Math.round(new Date().getTime() / 1000);
+        const endTime = Math.round((startTime + Math.round(duration * 60)) / 100) * 100;
+        const variables = {
+            isOrderAsk: true,
+            signer: signer.address,
+            collection: token.address,
+            price: price.toString(),
+            tokenId: token.id,
+            amount: '1',
+            strategy: STRATEGY,
+            currency: WETH,
+            nonce,
+            startTime,
+            endTime,
+            minPercentageToAsk: 8500,
+            params: [],
+        };
+        const signature = await signer._signTypedData(MAKER_ORDER_SIG_DOMAIN, MAKER_ORDER_SIG_TYPES, variables);
+        variables.signature = signature;
+
+        // Create graphql query
+        const body = {
+            query: '\n    mutation CreateOrderMutation($data: OrderCreateInput!) {\n      createOrder(data: $data) {\n        ...OrderFragment\n      }\n    }\n    \n  fragment OrderFragment on Order {\n    isOrderAsk\n    signer\n    collection {\n      address\n    }\n    price\n    amount\n    strategy\n    currency\n    nonce\n    startTime\n    endTime\n    minPercentageToAsk\n    params\n    signature\n    token {\n      tokenId\n    }\n    hash\n  }\n\n',
+            variables: { data: variables },
+        };
+
+        const res = await this.post(URL, body, HEADERS, 'json');
+        if (!res || !res.data || !res.data.createOrder) {
+            throw new Error('Response data invalid format');
+        }
+
+        const o = res.data.createOrder;
+        return {
+            hash: o.hash,
+            price: ethers.BigNumber.from(o.price),
+            nonce: parseInt(o.nonce),
+            endTime: new Date(parseInt(o.endTime) * 1000),
+        };
+    }
+
+    async getNonce(user) {
+        if (!user || !ethers.utils.isAddress(user)) {
+            throw new Error('Missing or invalid user');
+        }
+
+        const body = {
+            query: "\n    query GetUserNonce($address: Address!) {\n      user(address: $address) {\n        nonce\n      }\n    }\n",
+            variables: {
+                address: user
+            }
+        };
+
+        const res = await this.post(URL, body, HEADERS, 'json');
+        if (!res || !res.data || !res.data.user) {
+            throw new Error('Response data invalid format');
+        }
+
+        return parseInt(res.data.user.nonce);
+    }
+
+    async isTokenOwner(user, token) {
+        if (!user || !ethers.utils.isAddress(user)) {
+            throw new Error('Missing or invalid user');
+        }
+        if (!token) {
+            throw new Error('Missing token');
+        }
+
+        const body = {
+            query: "\n    query GetUserNonce($address: Address!) {\n      user(address: $address) {\n        nonce\n      }\n    }\n",
+            variables: {
+                address: user
             }
         };
         
-        try {
-            const res = await this.post(url, body, headers, 'json');
-            if (res && res.data && res.data.token) {
-                return {
-                    floorPrice: ethers.utils.parseEther(res.data.token.collection.floorOrder.price),
-                    floorPriceOS: ethers.utils.parseEther(res.data.token.collection.floor.floorPriceOS),
-                    price: res.data.token.ask ? ethers.utils.parseEther(res.data.token.ask.price) : null,
-                    listingNonce: res.data.token.ask ? res.data.token.ask.nonce : null,
-                };
-            }
-            else {
-                return {};
+        user = user.toLowerCase();
+        const res = await this.post(URL, body, HEADERS, 'json');
+        if (!res || !res.data || !res.data.token || !res.data.token.owners) {
+            throw new Error('Response data invalid format'); 
+        }
+
+        const owners = res.data.token.owners;
+        for (const spec of owners) {
+            if (spec.owner.address.toLowerCase() === user) {
+                return true;
             }
         }
-        catch (err) {
-            console.log('Failed to retrieve lookrare listing for ' + tokenContract + ' ' + tokenId + ': ' + err.message);
-            return {};
-        }
+        return false;
     }
 }
 
---
+++ tests/apps/farming/listing-investigation.js
@@ -0,0 +1,53 @@
+const ethers = require('ethers');
+const { Token } = require('../../../src/apps/farming/objects');
+const { getNftyHttpsProv, decodeTx, LooksRequests, readConfigs } = require('../../../src/utils');
+
+process.env.PATH_TO_CONFIGS = '/home/vdog/workspace/private/web3-utils/configs.json';
+
+// index.esm.js:formatted
+const mainnetAddresses = {
+    LOOKS: "0xf4d2888d29D722226FafA5d9B24F9164c092421E",
+    LOOKS_LP: "0xDC00bA87Cc2D99468f7f34BC04CBf72E111A32f7",
+    WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    ROYALTY_FEE_MANAGER: "0x7358182024c9f1B2e6b0153e60bf6156B7eF4906",
+    ROYALTY_FEE_REGISTRY: "0x55010472a93921a117aAD9b055c141060c8d8022",
+    ROYALTY_FEE_SETTER: "0x66466107d9cAE4da0176a699406419003F3C27a8",
+    EXCHANGE: "0x59728544B08AB483533076417FbBB2fD0B17CE3a",
+    TRANSFER_MANAGER_ERC721: "0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e",
+    TRANSFER_MANAGER_ERC1155: "0xFED24eC7E22f573c2e08AEF55aA6797Ca2b3A051",
+    TRANSFER_SELECTOR_NFT: "0x9Ba628F27aAc9B2D78A9f2Bf40A8a6DF4Ccd9e2c",
+    STRATEGY_STANDARD_SALE: "0x56244Bb70CbD3EA9Dc8007399F61dFC065190031",
+    STRATEGY_COLLECTION_SALE: "0x86F909F70813CdB1Bc733f4D97Dc6b03B8e7E8F3",
+    STRATEGY_PRIVATE_SALE: "0x58D83536D3EfeDB9F7f2A1Ec3BDaad2b1A4DD98C",
+    STRATEGY_DUTCH_AUCTION: "0x3E80795Cae5Ee215EBbDf518689467Bf4243BAe0",
+    PRIVATE_SALE_WITH_FEE_SHARING: "0x9571cdD8ACB71C83393290F0D63A46173dddE65B",
+    FEE_SHARING_SYSTEM: "0xBcD7254A1D759EFA08eC7c3291B2E85c5dCC12ce",
+    STAKING_POOL_FOR_LOOKS_LP: "0x2A70e7F51f6cd40C3E9956aa964137668cBfAdC5",
+    TOKEN_DISTRIBUTOR: "0x465A790B428268196865a3AE2648481ad7e0d3b1",
+    TRADING_REWARDS_DISTRIBUTOR: "0x453c1208B400fE47aCF275315F14E8F9F9fbC3cD",
+    MULTI_REWARDS_DISTRIBUTOR: "0x0554f068365eD43dcC98dcd7Fd7A8208a5638C72",
+    MULTICALL2: "0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696",
+    REVERSE_RECORDS: "0x3671aE578E63FdF66ad4F3E12CC0c0d71Ac7510C",
+    AGGREGATOR_UNISWAP_V3: "0x3ab16Af1315dc6C95F83Cbf522fecF98D00fd9ba",
+};
+
+async function main() {
+    const looksRequests = new LooksRequests(false); 
+    const { privateKeys } = await readConfigs();   
+    const prov = await getNftyHttpsProv();
+    const signer = new ethers.Wallet(privateKeys.vdog, prov);
+    // const res = await looksRequests.createListing(
+    //     signer, 
+    //     new Token('0xfb7d186E24E128be1F1339fB9C2bA6fDBd87C6f9', 14583), 
+    //     ethers.utils.parseEther('0.009'),
+    //     33,
+    //     1
+    // );
+    // console.log('Response:');
+    // console.log(JSON.stringify(res, null, 2));
+
+    const nonce = await looksRequests.getNonce(signer.address);
+    console.log('Nonce: ' + nonce);
+}
+
+main().catch(console.log);
